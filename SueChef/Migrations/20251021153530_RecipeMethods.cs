75a710d89d0f1bfc1f150494d5ebf9b06f67d47a
Karan Kullar <jaskaran.kullar.13@alumni.ucl.ac.uk>
2025-10-21 16:39:39 +0100

    Update Recipe Model - Db Migration

---
+++ SueChef/Migrations/20251021153530_RecipeMethods.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SueChef.Migrations
+{
+    /// <inheritdoc />
+    public partial class RecipeMethods : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Method",
+                table: "Recipes",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Method",
+                table: "Recipes");
+        }
+    }
+}
--- SueChef/Models/RecipeModel.cs
+++ SueChef/Models/RecipeModel.cs
@@ -9,6 +9,7 @@ public class Recipe
     public int Id { get; set; }
     public string? Title { get; set; }
     public string? Description { get; set; }
+    public string? Method { get; set; }
     public int Serving { get; set; }
     public int DifficultyLevel { get; set; }
     public bool IsVegetarian { get; set; }
