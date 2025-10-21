59ede520ee986767a7b2633be981678e95cb66b5
Karan Kullar <jaskaran.kullar.13@alumni.ucl.ac.uk>
2025-10-21 15:02:12 +0100

    Setting up playwright

--- SueChef.Tests/UnitTest1.cs
+++ SueChef.Tests/UnitTest1.cs
@@ -1,15 +1,21 @@
 ﻿namespace SueChef.Tests;
 
-public class Tests
-{
-    [SetUp]
-    public void Setup()
-    {
-    }
+using System.Text.RegularExpressions;
+using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+using NUnit.Framework;
 
+public class Tests : PageTest
+{
+    // the following method is a test
     [Test]
-    public void Test1()
+    public async Task IndexpageHasPlaywrightInTitleAndGetStartedLinkLinkingtoTheIntroPage()
     {
-        Assert.Pass();
+        // go to the MakersBnB Index page
+        // NB: update the localhost port with the one your server is running on 
+        await Page.GotoAsync("http://localhost:5179");
+
+        // expect the page title to contain "Index Page - MakersBnB"
+        await Expect(Page).ToHaveTitleAsync(new Regex("Home Page"));
     }
 }
