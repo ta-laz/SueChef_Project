294741aeddb433a68fb36df261cbcb48bc06b4b2
Sarah <sarah.ea.hunter@gmail.com>
2025-10-23 15:16:10 +0100

    Built New Meal Plan form within Index

---
+++ SueChef/Controllers/MealPlanController.cs
@@ -0,0 +1,39 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SueChef.Models;
+using SueChef.ViewModels;
+using SueChef.ActionFilters;
+
+
+namespace SueChef.Controllers;
+
+[ServiceFilter(typeof(AuthenticationFilter))]
+public class MealPlanController : Controller
+{
+    private readonly ILogger<MealPlanController> _logger;
+    private readonly SueChefDbContext _db;
+
+    public MealPlanController(ILogger<MealPlanController> logger, SueChefDbContext db)
+    {
+        _logger = logger;
+        _db = db;
+    }
+
+
+    [Route("/MealPlans")]
+    [HttpGet]
+    public async Task<IActionResult> Index()
+    {
+        return View();
+    }
+
+
+    [Route("/MealPlans")]
+    [HttpPost]
+    public async Task<IActionResult> Create()
+    {
+        return View(Index);
+    }
+
+}
