1320aca98c402ecfe77454a064e21ff48fdfc438
Sarah <sarah.ea.hunter@gmail.com>
2025-10-23 16:06:05 +0100

    Added validation to meal plan title

--- SueChef/Controllers/MealPlanController.cs
+++ SueChef/Controllers/MealPlanController.cs
@@ -25,16 +25,26 @@ public class MealPlanController : Controller
     [HttpGet]
     public async Task<IActionResult> Index()
     {
+        var MealPlans = await _db.MealPlans.OrderByDescending(mp => mp.UpdatedOn).ToListAsync();
         return View();
     }
 
 
     [Route("/MealPlans")]
     [HttpPost]
-    public async Task<IActionResult> Create(MealPlan mealPlan)
+    public async Task<IActionResult> Create(MealPlanViewModel mealPlanViewModel)
     {
         int currentUserId = HttpContext.Session.GetInt32("user_id").Value;
+        var mealPlan = new MealPlan();
+
+        if (await _db.MealPlans.AnyAsync(mp => mp.MealPlanTitle == mealPlanViewModel.MealPlanTitle))
+        {
+            ModelState.AddModelError("", "Meal Plan Title must be unique.");
+            return View("Index", mealPlanViewModel);
+        }
         mealPlan.UserId = currentUserId;
+        mealPlan.MealPlanTitle = mealPlanViewModel.MealPlanTitle;
+
         _db.MealPlans.Add(mealPlan);
         await _db.SaveChangesAsync();
         return View("Index");
---
+++ SueChef/ViewModels/MealPlanViewModel.cs
@@ -0,0 +1,14 @@
+namespace SueChef.Models;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data;
+
+public class MealPlanViewModel
+{
+    public int Id { get; set; }
+    public string? MealPlanTitle { get; set; }
+    public int RecipeCount { get; set; }
+    public DateOnly? UpdatedOn { get; set; }
+
+}
