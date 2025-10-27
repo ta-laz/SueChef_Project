b6061727247042f0315663318ff8239b82f0a694
Sarah <sarah.ea.hunter@gmail.com>
2025-10-27 20:14:41 +0000

    Revert "Everything is broken, trying to fix it"
    This reverts commit 0bbf959d43cf5dadb6af911ba826e2e5fdb6b0d6.

--- SueChef/ViewModels/IndividualRecipePageViewModel.cs
+++ SueChef/ViewModels/IndividualRecipePageViewModel.cs
@@ -5,5 +5,5 @@ public class IndividualRecipePageViewModel
     public IndividualRecipeViewModel IndividualRecipe { get; set; }
     public bool IsFavourited { get; set; }
     public List<MealPlanViewModel>? UserMealPlans { get; set; } = new();
-    // public bool IsSignedIn { get; set; }
+    public bool IsSignedIn { get; set; }
 }
