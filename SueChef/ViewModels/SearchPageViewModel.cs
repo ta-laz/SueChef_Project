0752e7006c7073e51011fafcc81c27123d0f67fb
Alex Lazar <teodoraalexandra.lazar@gmail.com>
2025-10-27 15:28:16 +0000

    Updated viewmodel with ingredientrs, categories and chefs

--- SueChef/ViewModels/SearchPageViewModel.cs
+++ SueChef/ViewModels/SearchPageViewModel.cs
@@ -4,5 +4,8 @@ namespace SueChef.ViewModels
     {
         public string? SearchQuery { get; set; }
         public List<RecipeCardViewModel> Recipes { get; set; } = new();
+        public List<string> AllIngredients { get; set; } = new();
+        public List<string> AllCategories { get; set; } = new();
+        public List<string> AllChefs { get; set; } = new();
     }
 }
