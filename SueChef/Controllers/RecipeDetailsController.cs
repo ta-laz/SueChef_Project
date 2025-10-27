dffd7f6da69d9ef3c874c4d164bf712b14d8f745
Sarah <sarah.ea.hunter@gmail.com>
2025-10-27 20:15:34 +0000

    Revert "Added redirect so that if the user tries to access dropdown when not signed in, redirects to signin with error message"
    This reverts commit 27db54117fc333cb93810a556898eb20e63757b8.

--- SueChef/Controllers/RecipeDetailsController.cs
+++ SueChef/Controllers/RecipeDetailsController.cs
@@ -92,7 +92,6 @@ public class RecipeDetailsController : Controller
         // Logic to collect all Meal Plans belonging to signed in user
         var mealPlans = await _db.MealPlans
             .Where(mp => mp.UserId == currentUserId)
-            .OrderByDescending(mp => mp.UpdatedOn)
             .Select(mp => new MealPlanViewModel
             {
                 Id = mp.Id,
@@ -105,8 +104,7 @@ public class RecipeDetailsController : Controller
         {
             IndividualRecipe = viewModel,
             IsFavourited = isFavourited,
-            UserMealPlans = mealPlans,
-            IsSignedIn = currentUserId != null
+            UserMealPlans = mealPlans
         };
         return View(AllViewModels);
 
