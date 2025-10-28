f2b28c8b72660fdc4cd81eaaf4cb5c012a8db6eb
Tom <tomwight233@gmail.com>
2025-10-28 14:35:06 +0000

    lots of tests for categories page

--- SueChef.Tests/CategoryPage.Test.cs
+++ SueChef.Tests/CategoryPage.Test.cs
@@ -39,7 +39,7 @@ public class CategoryPage : PageTest
     }
 
     [Test]
-    public async Task EasyCategoryParameter_CategoriesPage_ShowsOnlyEasyRecipes()
+    public async Task EasyCategoryPage_CategoriesPage_ShowsOnlyEasyRecipes()
     {
         await Page.GotoAsync("/Categories?category=easy");
         await Expect(Page.GetByTestId("Title-Easy Recipes")).ToBeVisibleAsync();
@@ -51,14 +51,14 @@ public class CategoryPage : PageTest
         {
             var difficultyLevelText = await recipeCards.Nth(i).Locator("[data-testid='difficulty-level']").TextContentAsync();
 
-            Assert.That("Easy", Is.EqualTo(difficultyLevelText.Trim()));
-            Assert.That("Medium", Is.Not.EqualTo(difficultyLevelText.Trim()));
-            Assert.That("Hard", Is.Not.EqualTo(difficultyLevelText.Trim()));
+            Assert.That(difficultyLevelText.Trim(), Is.EqualTo("Easy"));
+            Assert.That(difficultyLevelText.Trim(), Is.Not.EqualTo("Medium"));
+            Assert.That(difficultyLevelText.Trim(), Is.Not.EqualTo("Hard"));
         }
     }
 
     [Test]
-    public async Task MediumCategoryParameter_CategoriesPage_ShowsOnlyMediumRecipes()
+    public async Task MediumCategoryPage_CategoriesPage_ShowsOnlyMediumRecipes()
     {
         await Page.GotoAsync("/Categories?category=medium");
         await Expect(Page.GetByTestId("Title-Medium Recipes")).ToBeVisibleAsync();
@@ -70,14 +70,14 @@ public class CategoryPage : PageTest
         {
             var difficultyLevelText = await recipeCards.Nth(i).Locator("[data-testid='difficulty-level']").TextContentAsync();
 
-            Assert.That("Medium", Is.EqualTo(difficultyLevelText.Trim()));
-            Assert.That("Easy", Is.Not.EqualTo(difficultyLevelText.Trim()));
-            Assert.That("Hard", Is.Not.EqualTo(difficultyLevelText.Trim()));
+            Assert.That(difficultyLevelText.Trim(), Is.EqualTo("Medium"));
+            Assert.That(difficultyLevelText.Trim(), Is.Not.EqualTo("Easy"));
+            Assert.That(difficultyLevelText.Trim(), Is.Not.EqualTo("Hard"));
         }
     }
 
     [Test]
-    public async Task HardCategoryParameter_CategoriesPage_ShowsOnlyHardRecipes()
+    public async Task HardCategoryPage_CategoriesPage_ShowsOnlyHardRecipes()
     {
         await Page.GotoAsync("/Categories?category=hard");
         await Expect(Page.GetByTestId("Title-Hard Recipes")).ToBeVisibleAsync();
@@ -89,9 +89,97 @@ public class CategoryPage : PageTest
         {
             var difficultyLevelText = await recipeCards.Nth(i).Locator("[data-testid='difficulty-level']").TextContentAsync();
 
-            Assert.That("Hard", Is.EqualTo(difficultyLevelText.Trim()));
-            Assert.That("Easy", Is.Not.EqualTo(difficultyLevelText.Trim()));
-            Assert.That("Medium", Is.Not.EqualTo(difficultyLevelText.Trim()));
+            Assert.That(difficultyLevelText.Trim(), Is.EqualTo("Hard"));
+            Assert.That(difficultyLevelText.Trim(), Is.Not.EqualTo("Easy"));
+            Assert.That(difficultyLevelText.Trim(), Is.Not.EqualTo("Medium"));
         }
     }
+
+    [Test]
+    public async Task QuickCategoryPage_CategoriesPage_ShowsOnlyQuickRecipes()
+    {
+        await Page.GotoAsync("/Categories?category=quick");
+        await Expect(Page.GetByTestId("Title-Quick Recipes")).ToBeVisibleAsync();
+
+        var recipeCards = Page.Locator(".recipe-card");
+
+        var recipeCount = await recipeCards.CountAsync();
+        for (int i = 0; i < recipeCount; i++)
+        {
+            var recipeTimeText = await recipeCards.Nth(i).Locator("[data-testid='recipe-time']").TextContentAsync();
+
+            Assert.That(recipeTimeText.Trim(), Does.Not.Contain("hr"));
+        }
+    }
+
+    [Test]
+    public async Task HighlyRatedCategoryPage_CategoriesPage_ShowsHighestRatedRecipeFirst()
+    {
+        await Page.GotoAsync("/Categories?category=highlyrated");
+        await Expect(Page.GetByTestId("Title-Top 10 Recipes")).ToBeVisibleAsync();
+
+        // expect that the first recipe title on the page is the highest rated recipe
+        // this may change after seeds change
+        await Expect(Page.GetByTestId("recipe-title-div").First).ToContainTextAsync("Caprese Tomato Mozzarella Salad");
+    }
+
+    [Test]
+    public async Task MostPopularCategoryPage_CategoriesPage_ShowsMostPopularRecipeFirst()
+    {
+        await Page.GotoAsync("/Categories?category=mostpopular");
+        await Expect(Page.GetByTestId("Title-Most Popular Recipes")).ToBeVisibleAsync();
+
+        // expect that the first recipe title on the page is the most rated recipe
+        // this may change after seeds change
+        await Expect(Page.GetByTestId("recipe-title-div").First).ToContainTextAsync("Korean Bibimbap Rice Bowl");
+    }
+
+    [Test]
+    public async Task RateRecipe_CategoriesPage_UpdatesMostPopularRecipe()
+    {
+        await Page.GotoAsync("/Categories?category=mostpopular");
+        await Expect(Page.GetByTestId("Title-Most Popular Recipes")).ToBeVisibleAsync();
+
+        // expect that the first recipe title on the page is the most rated recipe
+        // this may change after seeds change
+        await Expect(Page.GetByTestId("recipe-title-div").First).ToContainTextAsync("Korean Bibimbap Rice Bowl");
+
+        await Page.GotoAsync($"{BaseUrl}/signup");
+        await Page.GetByTestId("username").FillAsync("TestingUser");
+        await Page.GetByTestId("dob").FillAsync("1995-08-10");
+        await Page.GetByTestId("email").FillAsync("test@testmail.com");
+        await Page.GetByTestId("password").FillAsync("Password123!");
+        await Page.GetByTestId("confirmpassword").FillAsync("Password123!");
+        await Page.GetByTestId("signup-submit").ClickAsync();
+
+        await Page.GotoAsync("/Recipe/29");
+        await Page.GetByTestId("star-rating-1").ClickAsync();
+        await Page.GetByTestId("rating-submit-button").ClickAsync();
+        await Expect(Page.GetByTestId("thanks-for-rating")).ToBeVisibleAsync();
+
+        await Page.GotoAsync("/Categories?category=mostpopular");
+        await Expect(Page.GetByTestId("Title-Most Popular Recipes")).ToBeVisibleAsync();
+
+        // expect that the first recipe title on the page is the recipe that's just been rated
+        // this may change after seeds change
+        await Expect(Page.GetByTestId("recipe-title-div").First).ToContainTextAsync("Oven-Baked Tandoori Salmon");
+    }
+
+    [Test]
+    public async Task DairyFreeCategoryPage_CategoriesPage_ShowsOnlyDairyFreeRecipe()
+    {
+        await Page.GotoAsync("/Categories?category=dairyfree");
+        await Expect(Page.GetByTestId("Title-Dairy-Free Recipes")).ToBeVisibleAsync();
+
+        var recipeCards = Page.Locator(".recipe-card");
+
+        var recipeCount = await recipeCards.CountAsync();
+        for (int i = 0; i < recipeCount; i++)
+        {
+            var dairyFreeText = await recipeCards.Nth(i).Locator("[data-testid='dairy-free-text']").IsVisibleAsync();
+        }
+    }
+
+    // , dairy free, vegetarian, all recipes
+
 }
